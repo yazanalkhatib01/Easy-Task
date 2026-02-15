18fdd53e59673e547f911d560fa455a84c64ee9c
Designer <Designer@DESKTOP-NG94IPO>
2026-02-15 11:07:32 +0300

    Feat:add new-task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -14,7 +9,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
   get imagePath() {
